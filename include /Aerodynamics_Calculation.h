34f6d8cdbdcd3ad174442217381a29257f863afa
Raempest <thehuman130410@gmail.com>
2026-05-18 21:45:09 +0500

    Fix errors

--- include /Aerodynamics_Calculation.h
+++ include /Aerodynamics_Calculation.h
@@ -1,21 +1,26 @@
-#ifndef AERODYNAMIC_CALCULATION_H
-#define AERODYNAMIC_CALCULATION_H
-class Aerodynamics_calculation(){
-    public:
-    Aerodynamics_calculation() = default
-    Aerodynamics_calculation(double CL, double CD, double CT,
-                             double rho, double v, double s,
-                             double w, double m, double g = 9.81);
+#ifndef AERODYNAMICS_CALCULATION_H
+#define AERODYNAMICS_CALCULATION_H
+
+class AerodynamicsCalculation {
+public:
+
+    AerodynamicsCalculation() = default;
+
+    AerodynamicsCalculation(double CL, double CD, double CT,
+                            double rho, double v, double S,
+                            double w, double m, double g = 9.81);
+
     void setLiftCoefficient(double value);
     void setDragCoefficient(double value);
     void setThrustCoefficient(double value);
+
     void setAirDensity(double value);
     void setVelocity(double value);
     void setArea(double value);
     void setAngularVelocity(double value);
     void setMass(double value);
     void setGravity(double value);
-    
+
     double getLiftCoefficient() const;
     double getDragCoefficient() const;
     double getThrustCoefficient() const;
@@ -33,16 +38,18 @@ class Aerodynamics_calculation(){
     double calculateWeight() const;
 
     bool isHoverCondition() const;
+
 private:
     double CL{0.0};
     double CD{0.0};
     double CT{0.0};
 
-    double rho{0.0};   
-    double v{0.0};     
-    double S{0.0};     
-    double w{0.0};     
-    double m{0.0};     
+    double rho{0.0};
+    double v{0.0};
+    double S{0.0};
+    double w{0.0};
+    double m{0.0};
     double g{9.81};
-        };
+};
+
 #endif
